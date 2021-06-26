fdd4a605692cd344f2b1c7102096ea5a8c6da723
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-06-26 01:26:24 -0300

    ✨ feat: fill date field

--- JucemgScrapping/Main.cs
+++ JucemgScrapping/Main.cs
@@ -9,20 +9,21 @@ namespace JucemgScrapping
     {
         public Main()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         [Obsolete]
-        private async void button1_Click(object sender, EventArgs e)
+        private async void Button1_Click(object sender, EventArgs e)
         {
-            string date = currentDate.Value.ToString("yyyy-MM-dd");
+            string date = currentDate.Value.AddDays(-1).ToString("yyyy-MM-dd");
             _ = await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
             var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = false
             });
             var page = await browser.NewPageAsync();
-            await page.GoToAsync("http://www.google.com");
+            await page.GoToAsync("https://jucemg.mg.gov.br/atos");            
+            await page.EvaluateFunctionAsync<dynamic>("(value)=> document.querySelector('input[type=date]').value = value",date);
 
         }
     }
