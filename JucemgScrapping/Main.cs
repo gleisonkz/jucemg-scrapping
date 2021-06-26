73602a38cebc6616074d97dee2e4eedb9f2c6a94
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-06-26 01:40:44 -0300

    ✨ feat: click to search

--- JucemgScrapping/Main.cs
+++ JucemgScrapping/Main.cs
@@ -1,5 +1,6 @@
 ﻿using PuppeteerSharp;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,7 +10,7 @@ namespace JucemgScrapping
     {
         public Main()
         {
-            InitializeComponent();            
+            InitializeComponent();
         }
 
         [Obsolete]
@@ -19,12 +20,31 @@ namespace JucemgScrapping
             _ = await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
             var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
-                Headless = false
+                Headless = false,
+                DefaultViewport = null
             });
             var page = await browser.NewPageAsync();
-            await page.GoToAsync("https://jucemg.mg.gov.br/atos");            
-            await page.EvaluateFunctionAsync<dynamic>("(value)=> document.querySelector('input[type=date]').value = value",date);
+            await page.GoToAsync("https://jucemg.mg.gov.br/atos");
+            await page.EvaluateFunctionAsync<dynamic>("(value)=> document.querySelector('input[type=date]').value = value", date);
 
+            var companies = new List<string>();
+
+            await ReplaceText(page, "#tp_processo_id", "EXTINÇÃO");
+            await page.WaitForSelectorAsync("#pesquisa_ato");
+            ClickAsync(page,"a#pesquisa_ato");
+            //await page.ClickAsync("a#pesquisa_ato");
+        }
+
+        private static async Task ReplaceText(Page page, string selector, string replacementValue)
+        {
+            await page.WaitForSelectorAsync(selector).ConfigureAwait(false);
+            await page.EvaluateExpressionAsync($"document.querySelector(\"{selector}\").value = \"{replacementValue}\"").ConfigureAwait(false);
+        }
+
+        private static async Task ClickAsync(Page page, string selector)
+        {
+            await page.WaitForSelectorAsync(selector).ConfigureAwait(false);
+            await page.EvaluateExpressionAsync($"document.querySelector(\"{selector}\").click()").ConfigureAwait(false);
         }
     }
 }
