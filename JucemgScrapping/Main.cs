6df598d2df0151c2488fed4b4180a3dcf116b5c8
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-06-26 01:13:00 -0300

    🎉 initial: basic setup

---
+++ JucemgScrapping/Main.cs
@@ -0,0 +1,29 @@
+﻿using PuppeteerSharp;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JucemgScrapping
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        [Obsolete]
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string date = currentDate.Value.ToString("yyyy-MM-dd");
+            _ = await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = false
+            });
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("http://www.google.com");
+
+        }
+    }
+}
